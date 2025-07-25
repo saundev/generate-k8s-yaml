7aed6a1c9feeb883af4858c58f6c7965485c6a32
TastyWitch <145932873+TastyWitch@users.noreply.github.com>
2025-07-25 16:31:13 +0100

    Update generate-k8s-yaml.sh
    Consistent indent at two spaces.

--- generate-k8s-yaml.sh
+++ generate-k8s-yaml.sh
@@ -2,13 +2,13 @@
 ###############
 ### SaunDev ###
 ###############
-### Note ComponentStatus (cs) as it's deprecated - Also while (k get all) is shorter it does not get all resources such as secrets, etc.
+### Note that ComponentStatus (cs) is deprecated so removed from list - Also while (k get all) is shorter it does not get all resources such as secrets, etc.
 for ns in $(k get ns -o=jsonpath='{.items[*].metadata.name}' | tr ' ' '\n')
 do
-    for resource in $(k get cj,cm,crd,deploy,ds,hpa,ingress,ip,job,node,pod,pv,pvc,sa,sc,secret,statefulset,svc -o=name -n=$ns)
-    do
-        outdir=$(dirname $ns/$resource)
-        mkdir -p "$outdir"
-        k get $resource -n=$ns -o=yaml > "$ns/$resource.yaml"
-    done
+  for resource in $(k get cj,cm,crd,deploy,ds,hpa,ingress,ip,job,node,pod,pv,pvc,sa,sc,secret,statefulset,svc -o=name -n=$ns)
+  do
+    outdir=$(dirname $ns/$resource)
+    mkdir -p "$outdir"
+    k get $resource -n=$ns -o=yaml > "$ns/$resource.yaml"
+  done
 done
