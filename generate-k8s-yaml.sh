0cbdafd8ec417d7ab6fc3bf980ed2a37a9a5f989
TastyWitch <145932873+TastyWitch@users.noreply.github.com>
2025-08-01 09:05:12 +0100

    Update generate-k8s-yaml.sh

--- generate-k8s-yaml.sh
+++ generate-k8s-yaml.sh
@@ -2,10 +2,10 @@
 ###############
 ### SaunDev ###
 ###############
-### Note that ComponentStatus (cs) is deprecated so removed from list - Also while (k get all) is shorter it does not get all resources such as secrets, etc.
+### Note ComponentStatus (cs) as it's deprecated - Also while (k get all) is shorter it does not get all resources such as secrets, etc.
 for ns in $(k get ns -o=jsonpath='{.items[*].metadata.name}' | tr ' ' '\n')
 do
-  for resource in $(k get cj,cm,crd,deploy,ds,hpa,ingress,ip,job,node,pod,pv,pvc,sa,sc,secret,statefulset,svc -o=name -n=$ns)
+  for resource in $(k get cj,cm,clusterroles,crd,csr,deploy,ds,hpa,ingress,ip,job,mutatingwebhookconfigurations,node,pod,pv,pvc,roles,sa,sc,secret,statefulset,svc,validatingwebhookconfigurations -o=name -n=$ns)
   do
     outdir=$(dirname $ns/$resource)
     mkdir -p "$outdir"
